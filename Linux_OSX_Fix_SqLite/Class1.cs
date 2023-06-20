4c6a5d469db371d010029e32cc0a45e0e6ff4e84
redpolline <137227028+redpolline@users.noreply.github.com>
2023-06-20 05:08:10 -0400

    Linux / OSX Build Hack: Create new empty netstandard2.0 project that pulls in System.Data.SQLite, and have other projects pull their SQLite.Interop.dll from it.
    New project: Linux_OSX_Fix_SqLite
    
    A hack if there's ever been one.
    
    This project contains no useful code beyond a netstandard2.0 and System.Data.SQLite dep.
    
    It exists solely to workaround MonoDevelop's inability to link ASP.NET, Mysql.Data, and System.Data.SQLite together in the same netstandard2.0 project.
    (As such, no other project in the solution declares a dependency on it for MSBuild to enforce... but they *will* fail to build if the new project doesn't pull in it's nuget packages first.)
    
    When all other projects have been updated to work with Microsoft's dotnet SDK, (i.e. When the ASP.NET dependant projects get converted to ASP.NET Core), this new project can be deleted.

---
+++ Linux_OSX_Fix_SqLite/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace Linux_OSX_Fix_SqLite
+{
+    public class Class1
+    {
+        public System.Enum Class_Test()
+        {
+            return SQLiteErrorCode.Constraint_Unique;
+        }
+    }
+}
